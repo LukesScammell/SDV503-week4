971b42c6325c7bd4753658115fbbfb6866c546b5
LukesScammell <Luke-Scammell@live.nmit.ac.nz>
2025-03-12 10:54:17 +1300

    Data types and notes

---
+++ app.js
@@ -0,0 +1,89 @@
+// const firstName = 'Ali'
+// const lastName = 'kahwaji'
+// Camel case naming
+// const classCode = 'SDV503'
+
+//Invalid vars
+//first-name
+//1_num
+//num_#_1
+
+// Declaring different variables of different data types
+
+let firstName = 'Luke'
+let lastName = 'Scammell'
+let country = 'New Zealand'
+let age = 18
+let isStudent = true
+
+console.log(firstName, lastName, country, age, 
+    isStudent
+)
+
+// Declaring variables with number values
+
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14
+console.log(gravity, boilingPoint,PI)
+
+//Variables can also be declaring in one line separated by comma
+
+let name = 'Luke', job = 'student', live = 'Richmond'
+console.log(name, job, live)
+
+/**
+ Exercise - 1: Variables 
+1- Declare four variables without assigning values
+
+2- Declare four variables with assigning values
+
+3- Declare variables to store your first name, last name,
+ student status, country and age in multiple lines.
+
+Declare variables to store your first name, last name, 
+ student status, country and age in a single line.
+
+Declare two variables myAge and yourAge and assign them initial
+ values and log to browser console. Output:
+
+I am 25 years old.
+You are 30 years old.
+ * 
+ */
+var city 
+var school
+var nmit
+var car
+
+var tutor = 'Ali'
+var catName = 'Tom'
+var laptop = 'HP'
+var weather = 'sunny'
+
+// TODO 
+/*
+Data Types 
+
+1- Primitive data types
+2- Non-primitive data types (Object Refrences )
+
+
+//SECTION - Primative Data Types:
+Means primitive data types are immutable data ypes
+1- Numbers (7 or 7.1)
+2- Strings (Any data under single or double quote)
+3- Booleans (true or false)
+4- Null (empty value or no value)
+5- undefined (a declared variable without a value)\
+
+//SECTION - Object Refrences 
+Means Object Refrences are mutable.
+1- Objects 
+2- Functions 
+3- Arrays 
+Ex: let nums = [1,2,3]
+    console.log(nums)
+    nums[2] = 10
+    console.log(nums)
+ */
